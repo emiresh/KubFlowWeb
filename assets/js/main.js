89263f7abd9b00243ff8c095dfd400fa1c61b5cd
Iresh Ekanayaka <ireshek@gmail.com>
2023-06-10 10:54:34 +0530

    changed

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,10 +20,10 @@
 
     if (window.pageYOffset > sticky) {
       header_navbar.classList.add("sticky");
-      logo.src = "assets/img/logo/escala-logo.png";
+      logo.src = "assets/img/logo/kubflow-logo.png";
     } else {
       header_navbar.classList.remove("sticky");
-      logo.src = "assets/img/logo/escala-logo.png";
+      logo.src = "assets/img/logo/kubflow-logo.png";
     }
 
     // show or hide the back-top-top button
